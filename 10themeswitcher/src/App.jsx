47968d278a0f6020e98967e2c59229e82e70d13a
Naresh Kumar <nareshkumar0000002@gmail.com>
2024-12-19 19:45:46 +0530

    contextapi

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -2,28 +2,45 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
+import ThemeBtn from './components/ThemeButton'
 
 function App() {
-  
+const {themeMOde,setThemeMode} = useState("light")
+const lightTheme = () => {
+  setThemeMode("light")
+}
 
+const darkTheme = () => {
+  setThemeMode("dark")
+}
+
+// actual change in theme
+useEffect (()=>{
+  document.querySelector('html').classList.remove("light","dark")
+  document.querySelector('html').classList.add(themeMOde)
+
+},[themeMOde])
 
 
   return (
-  
+    <ThemeProvider value ={{themeMOde,lightTheme,darkTheme}}>
+
+
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                      {/* theme Button*/}
+                      <ThemeBtn/>
                         
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                      {/* Card*/}
-                       
+                       <Card/>
                     </div>
                 </div>
             </div>
 
+            </ThemeProvider>
   )
 }
 
